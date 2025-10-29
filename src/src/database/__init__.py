5460d59dbbf45eb116379536687478801e0ef4eb
frrtune <bdrdria@yandex.ru>
2025-10-29 19:57:08 +0300

    Update __init__.py

--- src/src/database/__init__.py
+++ src/src/database/__init__.py
@@ -1,1 +1,23 @@
+"""
+Пакет для работы с векторной базой данных общежитий МИФИ
+"""
 
+from .base import (
+    create_database,
+    add_preloaded_data,
+    add_manual_data,
+    search_data,
+    view_all_data,
+    model
+)
+
+__all__ = [
+    'create_database',
+    'add_preloaded_data', 
+    'add_manual_data',
+    'search_data',
+    'view_all_data',
+    'model'
+]
+
+__version__ = '1.0.0'
